eea8727dfa2419ad4f6cf5d46cb0f01baf022e4f
og <oguogu@test.com>
2020-05-09 10:58:41 +0900

    [update] Playerクラスのリファクタリング

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,12 +9,9 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _jumpforce = 5.0f;
     [SerializeField]
-    private bool _grounded = false;
-
-    [SerializeField]
     private LayerMask _groundLayer;
 
-    private bool resetJumpNeeded = false;
+    private bool _resetJump = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,31 +21,43 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float move = Input.GetAxis("Horizontal");
+        var move = Movement();
 
-        
-        if (Input.GetKeyDown(KeyCode.Space) && _grounded == true)
-        {
-            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpforce);
-            _grounded = false;
-            resetJumpNeeded = true;
-            StartCoroutine(ResetJumpNeeded());
-        }
-
-        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, _groundLayer.value);
-        if (hitInfo.collider != null)
-        {
-            if(resetJumpNeeded == false)
-                _grounded = true;
-        }
 
         _rigid.velocity = new Vector2(move, _rigid.velocity.y);
     }
 
+    
+
+    private float Movement()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpforce);
+            StartCoroutine(ResetJumpNeeded());
+        }
+
+        return move;
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, _groundLayer.value);
+        if (hitInfo.collider != null)
+        {
+            if (_resetJump == false)
+                return true;
+        }
+        return false;
+    }
+    
     IEnumerator ResetJumpNeeded()
     {
+        _resetJump = true;
         yield return new WaitForSeconds(0.1f);
-        resetJumpNeeded = false;
+        _resetJump = false;
     }
     
 }
