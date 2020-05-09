6ebc69a0b5768e5eb836ee691c1c9d74f0274460
og <oguogu@test.com>
2020-05-09 10:49:09 +0900

    [update] プレイヤーのジャンプを実装
    スペースキーでジャンプ
    レイヤーにGroundを追加

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D _rigid;
+
+    [SerializeField]
+    private float _jumpforce = 5.0f;
+    [SerializeField]
+    private bool _grounded = false;
+
+    [SerializeField]
+    private LayerMask _groundLayer;
+
+    private bool resetJumpNeeded = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +24,31 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
+        float move = Input.GetAxis("Horizontal");
+
         
-        _rigid.velocity = new Vector2(horizontal, _rigid.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space) && _grounded == true)
+        {
+            _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpforce);
+            _grounded = false;
+            resetJumpNeeded = true;
+            StartCoroutine(ResetJumpNeeded());
+        }
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, _groundLayer.value);
+        if (hitInfo.collider != null)
+        {
+            if(resetJumpNeeded == false)
+                _grounded = true;
+        }
+
+        _rigid.velocity = new Vector2(move, _rigid.velocity.y);
     }
+
+    IEnumerator ResetJumpNeeded()
+    {
+        yield return new WaitForSeconds(0.1f);
+        resetJumpNeeded = false;
+    }
+    
 }
