7c6c4701edd5fe359c4ec4647992a8efcfafdca6
og <oguogu@test.com>
2020-05-09 10:25:17 +0900

    [update] Playerクラスに移動処理を実装

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D _rigid;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
         
+        _rigid.velocity = new Vector2(horizontal, _rigid.velocity.y);
     }
 }
