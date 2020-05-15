cd2e9ea96a7ba9733bf668f1a41f20e17b1995c1
og <oguogu@test.com>
2020-05-15 13:55:39 +0900

    [update] SpiderのMovementを実装

--- Assets/Scripts/Enemy/Spider.cs
+++ Assets/Scripts/Enemy/Spider.cs
@@ -1,18 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Spider : Enemy
 {
+
+    private Vector3 _currentTarget;
+    [SerializeField]
+    private Animator _anim;
+    [SerializeField]
+    private SpriteRenderer _spiderSprite;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            return;
+        }
+        
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (_currentTarget == pointA.position)
+        {
+            _spiderSprite.flipX = true;
+        }
+        else
+        {
+            _spiderSprite.flipX = false;
+        }
+        
+        if (transform.position == pointA.position)
+        {
+            _currentTarget = pointB.position;
+            _anim.SetTrigger("Idle");
+        }
+        else if (transform.position == pointB.position)
+        {
+            _currentTarget = pointA.position;
+            _anim.SetTrigger("Idle");
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
         
     }
 }
