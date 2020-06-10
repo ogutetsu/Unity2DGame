31295ce99f3fe09cf94ae654b170dd8c1119a043
og <oguogu@test.com>
2020-06-10 10:36:20 +0900

    [add] UIManagerクラスを追加
    OpenShopメソッドでPlayerのGem数を表示させるようにしました。

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+
+    public static UIManager Instance
+    {
+        get => _instance;
+        set => _instance = value;
+    }
+
+    public Text playerGemCountText;
+
+    public void OpenShop(int gemCount)
+    {
+        playerGemCountText.text = gemCount.ToString() + "G";
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+}
