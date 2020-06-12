95e2765c0b279e7b8b947445698647bba318d145
og <oguogu@test.com>
2020-06-12 13:45:45 +0900

    [update] StartButtonとQuitButtonの実装

--- Assets/Scripts/MinaMenu.cs
+++ Assets/Scripts/MinaMenu.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MinaMenu : MonoBehaviour
 {
+    
     public void StartButton()
     {
-        
+        SceneManager.LoadScene("Game");
     }
 
     public void QuitButton()
     {
-        
+        Application.Quit();
     }
 
 }
