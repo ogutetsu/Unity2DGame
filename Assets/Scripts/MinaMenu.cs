4f206796ad16815124bd7efe2d862956ef4862d6
og <oguogu@test.com>
2020-06-12 13:42:54 +0900

    [add] MainMenuクラスを追加

---
+++ Assets/Scripts/MinaMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinaMenu : MonoBehaviour
+{
+    public void StartButton()
+    {
+        
+    }
+
+    public void QuitButton()
+    {
+        
+    }
+
+}
